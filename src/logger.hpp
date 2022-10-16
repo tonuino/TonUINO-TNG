2df328ef029155ae3aca87081e6b0055d2824dc2
Thorsten Voß <voss@bummel.de>
2022-10-16 14:09:57 +0200

    Fix for pause/resume when card removed and returned (needs to be activated in admin menu)

--- src/logger.hpp
+++ src/logger.hpp
@@ -3,16 +3,16 @@
 
 #include "log.hpp"
 
-DEFINE_LOGGER(tonuino_log, s_info, void);
+DEFINE_LOGGER(tonuino_log, s_debug, void);
 
 DEFINE_LOGGER(init_log, s_info, tonuino_log);
-DEFINE_LOGGER(card_log, s_info, tonuino_log);
+DEFINE_LOGGER(card_log, s_debug, tonuino_log);
 DEFINE_LOGGER(play_log, s_warning, tonuino_log);
 DEFINE_LOGGER(standby_log, s_warning, tonuino_log);
-DEFINE_LOGGER(state_log, s_info, tonuino_log);
+DEFINE_LOGGER(state_log, s_debug, tonuino_log);
 DEFINE_LOGGER(button_log, s_warning, tonuino_log);
 DEFINE_LOGGER(modifier_log, s_warning, tonuino_log);
 DEFINE_LOGGER(mp3_log, s_warning, tonuino_log);
-DEFINE_LOGGER(settings_log, s_warning, tonuino_log);
+DEFINE_LOGGER(settings_log, s_debug, tonuino_log);
 
 #endif /* SRC_LOGGER_HPP_ */
