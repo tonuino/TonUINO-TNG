658724a1a9ab705c2f202f5bd773adead68c19b4
boerge1 <git@kalks.de>
2022-12-18 19:16:20 +0100

    Issue 028: Revise Button behavior
    * revert change in state machine (admin menu)

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1053,8 +1053,8 @@ void Admin_Entry::entry() {
 }
 
 void Admin_Entry::react(command_e const &cmd_e) {
-//  if (not chip_card.isCardRemoved())
-//    return;
+  if (not chip_card.isCardRemoved())
+    return;
 
   if (cmd_e.cmd_raw != commandRaw::none) {
     LOG(state_log, s_debug, str_Admin_Entry(), F("::react() "), static_cast<int>(cmd_e.cmd_raw));
