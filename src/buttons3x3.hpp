6061f5f30897cad461343395bfb15c8308a9330a
boerge1 <git@kalks.de>
2023-01-30 17:10:44 +0100

    Issue 019: Implement support for the 3x3 Button board
    * set levels also for classic and AiO

--- src/buttons3x3.hpp
+++ src/buttons3x3.hpp
@@ -11,16 +11,42 @@ class Buttons3x3: public CommandSource {
 public:
   static constexpr uint8_t numLevels = 9;
   static constexpr int levels[numLevels+1] = {
-                                           0
-                                       ,  77
-                                       , 148
-                                       , 234
-                                       , 306
-                                       , 390
-                                       , 465
-                                       , 534
-                                       , 593
-                                       , 784
+#ifdef TonUINO_Classic
+                                            0
+                                       ,   92
+                                       ,  194
+                                       ,  307
+                                       ,  405
+                                       ,  512
+                                       ,  608
+                                       ,  698
+                                       ,  773
+                                       , 1023
+#endif
+#ifdef ALLinONE
+                                            0
+                                       ,  490
+                                       ,  945
+                                       , 1494
+                                       , 1949
+                                       , 2480
+                                       , 2937
+                                       , 3373
+                                       , 3691
+                                       , 4064
+#endif
+#ifdef ALLinONE_Plus
+                                            0
+                                       ,   77
+                                       ,  148
+                                       ,  234
+                                       ,  306
+                                       ,  390
+                                       ,  465
+                                       ,  534
+                                       ,  593
+                                       ,  784
+#endif
                                        };
 
   Buttons3x3();
