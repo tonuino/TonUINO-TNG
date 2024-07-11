5b3283bc82d21ca4439e4ae12b24a8c74a09a636
ren-ke <typ.holzschuh0z@icloud.com>
2024-07-11 17:38:03 +0200

    Update constants.hpp

--- src/constants.hpp
+++ src/constants.hpp
@@ -71,7 +71,7 @@
 /* Select the right PCB by uncommenting one of the following lines
  * Bitte die passende Platine durch entfernen der Kommentare in einer der folgenden Zeilen auswählen
  */
-//#define TonUINO_Classic
+#define TonUINO_Classic
 //#define TonUINO_Every
 //#define ALLinONE
 //#define ALLinONE_Plus
@@ -84,7 +84,7 @@
  *          FIVEBUTTONS  for AiO and AiO+
  */
 //#define THREEBUTTONS
-//#define FIVEBUTTONS
+#define FIVEBUTTONS
 //#define BUTTONS3X3
 
 // ######################################################################
