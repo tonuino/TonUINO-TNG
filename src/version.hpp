45f881c0129c6d159627931eeac20ac57ae9edb5
boerge1 <git@kalks.de>
2025-10-02 15:11:25 +0200

    Issue_276: Deploy also binary for Esp32

--- src/version.hpp
+++ src/version.hpp
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-const char TONUINO_TNG_VERSION[] PROGMEM = "V3.3.1 22.07.25";
+const char TONUINO_TNG_VERSION[] PROGMEM = "V3.3.1 02.10.25";
 
 
 #endif /* SRC_VERSION_HPP_ */
